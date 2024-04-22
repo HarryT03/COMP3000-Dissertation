70da6969a1a62ba46c5384139271f53548896fe4
HarryT03 <harry.troake@students.plymouth.ac.uk>
2024-04-22 22:53:42 +0100

    Final Project

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -21,7 +21,7 @@ const recipeSchema = new mongoose.Schema({
     },
     category : {
         type: String,
-        enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian'],
+        enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian', 'Spanish'],
         required: 'This field is required',
     },
     image : {
